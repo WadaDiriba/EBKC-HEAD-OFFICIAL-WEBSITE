3ed693d9dc648e1e0e6b47399cf9538fb1342a03
WadaDiriba <wedadiriba@gmail.com>
2025-12-23 12:28:58 +0900

    something in the code udated

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-
-
-
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import About from "./components/about/about";
 import Campus from "./components/Campus/Campus";
 import Library from "./components/Librarys/Library";
-import Contact from "./components/Contact/contact";
 import Testmonial from "./components/Testmonial/Testmonial";
-import About from "./components/about/about";
+import Donate from "./components/Donate/Donate";
+import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="bg-gray-100">
-      {/* Navbar fixed at top */}
+    <>
+      {/* Fixed Navbar */}
       <Navbar />
 
-      {/* Main Sections */}
-      <Home />
-      <About />
-      <Campus />
-      <Library />
-      <Testmonial />
-      <Contact />
+      {/* Page Sections */}
+      <main style={{ paddingTop: "80px" }}>
+        <Home />
+        <About />
+        <Campus />
+        <Library />
+        <Testmonial />
+        <Donate />
+        <Contact />
+      </main>
 
       {/* Footer */}
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Donate/Donate.tsx
@@ -0,0 +1,11 @@
+export default function Donate() {
+
+    return (
+
+        <div style={{paddingTop:'100px', paddingBottom:'100px'}}>
+           
+        </div>  
+
+
+    );
+}
